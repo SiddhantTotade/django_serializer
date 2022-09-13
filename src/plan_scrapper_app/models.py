9376e8d81abbaf8185db4520bfe57e7334da4434
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-09-13 15:41:35 +0530

    changes in directory

---
+++ src/plan_scrapper_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Plans(models.Model):
+    monthly_rental = models.IntegerField()
+    data_with_rollover = models.IntegerField()
+    sms_per_day = models.IntegerField()
+    local_std_roaming = models.CharField(max_length=15)
+    amazon_prime = models.CharField(max_length=10)
--- src/scrapping_plans/scrapping_plans_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScrappingPlansAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scrapping_plans_app'
